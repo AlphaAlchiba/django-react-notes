3e0c8157d1df618411f65ccebe22af86feabe6c1
Alpha <alpha.alchiba@gmail.com>
2024-05-30 12:04:03 +0200

    developed more of the front end

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
-import react from "react"
+import react from "react";
+import { BrowserRouter, Routes, Route, Navigation } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
-
-  return (
-    <>
-
-    </>
-  )
+  return <></>;
 }
 
-export default App  
+export default App;
